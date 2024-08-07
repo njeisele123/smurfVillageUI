aad9d680d5008c4dc68c289989ea145cc475b2e7
njeisele <nickolas_eisele@alumni.brown.edu>
2024-08-06 22:31:41 -0400

    cleaned up accounts page and added dark mode

--- src/components/summoners/summoners.tsx
+++ src/components/summoners/summoners.tsx
@@ -10,7 +10,25 @@ import {
   deleteAccounts,
   getAccounts,
 } from "../../clients/summonerClient";
-import "../../index.css"
+import "../../index.css";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { PlusIcon } from "@heroicons/react/24/solid";
+
+const inputStyle = `w-64 
+px-4
+py-2
+my-4
+mr-2
+rounded-lg
+border
+bg-white dark:bg-gray-700
+text-gray-700 dark:text-gray-200
+border-gray-300 dark:border-gray-600
+focus:border-sky-300 dark:focus:border-sky-300
+focus:outline-none
+focus:ring-2
+focus:ring-sky-300 dark:focus:ring-sky-300
+placeholder-gray-400 dark:placeholder-gray-400`;
 
 export function Accounts() {
   const [accounts, setAccounts] = useState<AccountInfo[]>([]);
@@ -26,11 +44,15 @@ export function Accounts() {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold underline">Hello world!</h1>
+    <div
+      className="bg-white dark:bg-gray-800 text-black dark:text-white"
+      style={{ display: "inline-block" }}
+    >
+      <h1 className="text-2xl font-bold underline">Accounts</h1>
       {accounts?.map((acc, idx) => (
-        <div key={idx}>
+        <div key={idx} className="flex items-center">
           <input
+            className={inputStyle}
             value={acc.summoner_name}
             onChange={(e) => {
               accounts[idx].summoner_name = e.target.value;
@@ -38,38 +60,65 @@ export function Accounts() {
             }}
           />
           <input
+            className={inputStyle}
             value={acc.tag_line}
             onChange={(e) => {
               accounts[idx].tag_line = e.target.value;
               setAccounts([...accounts]);
             }}
           />
-          <button
+          <div
+            className="flex items-center justify-center w-10 h-10 ml-2"
             onClick={() => {
               accounts.splice(idx, 1);
               setAccounts([...accounts]);
             }}
           >
-            -
-          </button>
+            <TrashIcon className="h-6 w-6 ml-1 text-gray-500 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-400 cursor-pointer" />{" "}
+          </div>
         </div>
       ))}
-      <button
-        onClick={() => {
-          setAccounts([...accounts, { summoner_name: "", tag_line: "" }]);
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          // TODO: could do this more cleanly but easy to just delete all previous and add all new
-          deleteAccounts();
-          addAccounts(accounts);
-        }}
-      >
-        Save
-      </button>
+       <div className="w-full">
+        <button
+          className="
+          mt-4
+        mx-auto
+        flex items-center justify-center
+        w-10 h-10
+        bg-blue-500 hover:bg-blue-600
+        text-white
+        rounded-full
+        focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50
+        transition-colors duration-200
+      "
+          onClick={() => {
+            setAccounts([...accounts, { summoner_name: "", tag_line: "" }]);
+          }}
+        >
+          <PlusIcon className="h-6 w-6" />
+        </button>
+      </div>
+      <div className="w-full">
+        <button
+          className="
+          flex items-center justify-center
+          px-4 py-2
+          bg-green-500 hover:bg-green-600
+          text-white
+          rounded-lg
+          focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50
+          transition-colors duration-200
+        "
+          style={{ float: "right" }}
+          onClick={() => {
+            // TODO: could do this more cleanly but easy to just delete all previous and add all new
+            deleteAccounts();
+            addAccounts(accounts);
+          }}
+        >
+          Save
+        </button>
+      </div>
     </div>
   );
 }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,15 +1,21 @@
+import { useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
+
+  useEffect(() => {
+    document.documentElement.classList.add('dark');
+  }, []);
+
   return (
     <>
-      <div id="sidebar">
+      <div id="sidebar" className="bg-white dark:bg-gray-800 text-black dark:text-white">
         <nav>
           <Link to={`accounts`}>Accounts</Link>
           <Link to={`village`}>Village</Link>
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail" className="bg-white dark:bg-gray-800 text-black dark:text-white">
         <Outlet />
       </div>
     </>
